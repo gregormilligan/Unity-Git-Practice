d2f8e0d436bae2373a2420b424d482ad5c6b0588
Gregor Milligan <gregormilligan123@gmail.com>
2021-03-15 22:59:58 +0000

    2D and 3D rays
    2d rays working, tried to get 3d scene in but it didn't really work too well, oh well

---
+++ ExampleGame/Assets/Scripts/NewTestRaycasting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newTestRaycasting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitinfo;
+
+        if (Physics.Raycast(ray, out hitinfo))
+        {
+            Debug.DrawLine(ray.origin, hitinfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, hitinfo.point * 100, Color.green);
+        }
+        
+
+
+    }
+}
